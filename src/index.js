7ce538a82e3d8e39bb7fbeb8b87e800f9f95a9f2
Martin Maiaru <tincho_91_15@hotmail.com>
2023-02-15 23:39:55 -0300

    Plantillas + Websocket - 1

--- src/index.js
+++ src/index.js
@@ -5,9 +5,18 @@ import { __dirname } from "./path.js";
 import multer from "multer";
 import { engine } from "express-handlebars";
 import * as path from "path";
+import { Server } from "socket.io";
+import fs from "fs";
 
 const app = express();
 const port = 8080;
+
+const server = app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+const io = new Server(server);
+
 
 //Multer
 const storage = multer.diskStorage({
@@ -30,12 +39,28 @@ app.set("view engine", "handlebars");
 app.set("views", path.resolve(__dirname, "./views"));
 
 
+//Socket.io
+io.on("connection", (socket) => {
+  console.log("Conexion con socket")
+
+  socket.on("mensaje", info => { //Captura de info de cliente
+    console.log(info)
+  })
+
+  socket.broadcast.emit("evento-admin", "Hola desde server, sos el admin");
+
+  socket.emit("evento-general", "Hola a todos/as los/las usuarios/as");
+});
+
+
 //Routes
 app.use("/", express.static(__dirname + "/public"));
 app.use("/api/carts", cartRoutes);
 app.use("/api/products", productRoutes);
 
 app.get("/", (req,res) => {
+  const jsonContent = fs.readFileSync("src/data/products.json");
+  const products = JSON.parse(jsonContent);
 
   const user = {
     nombre: "Martin",
@@ -48,20 +73,30 @@ app.get("/", (req,res) => {
     {numComision: 123654, dias: "Martes y Jueves", horario: "18 a 20"}
   ]
 
-  res.render("home" , {
-    title: "MI SERVER",
-    mensaje: "MUNDO!",
-    isAdmin: user.role === "admin",
-    user,
-    cursos
-  })
-})
+  res.render("home", {
+    title: "My Server",
+    mensaje: "Bienvenido",
+    isAdmin: true,
+    user: {
+      nombre: "Martin",
+      email: "martin@example.com",
+      role: "admin"
+    },
+    products
+  });
+});
 
 app.post ('/upload', upload.single('product'), (req, res) => {
   console.log(req.file)
   res.send("Imagen Cargada")
 });
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.get("/realtimeproducts", (req, res) => {
+  const jsonContent = fs.readFileSync("src/data/products.json");
+  const products = JSON.parse(jsonContent);
+
+  res.render("realTimeProducts", {
+    title: "Real Time Products",
+    products
+  });
 });
--- src/routes/products.js
+++ src/routes/products.js
@@ -37,6 +37,7 @@ productRoutes.post("/", async (req, res) => {
   try {
     const newProduct = await productManager.addProduct(req.body);
     res.send(newProduct);
+    io.emit("product-added", newProduct);
   } catch (error) {
     console.error(error);
     res
